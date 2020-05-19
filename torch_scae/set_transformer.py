02be5148ef66d9f0b292aca1dceadcc636c849f7
Barış Deniz Sağlam <bdsaglam@gmail.com>
2020-05-19 09:15:57 +0300

    Implemented set transformer.

---
+++ torch_scae/set_transformer.py
@@ -0,0 +1,207 @@
+import math
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+# Attention blocks code credits https://github.com/juho-lee/set_transformer
+
+def qkv_attention(queries, keys, values, presence=None):
+    """
+    Transformer-like self-attention.
+
+    Args:
+      queries: Tensor of shape [B, N, d_k].
+      keys: Tensor of shape [B, M, d_k].
+      values: : Tensor of shape [B, M, d_v].
+      presence: None or tensor of shape [B, M].
+
+    Returns:
+      Tensor of shape [B, N, d_v]
+    """
+
+    d_k = queries.shape[-1]
+
+    # [B, N, d_k] x [B, d_k, M] = [B, N, M]
+    routing = torch.matmul(queries, keys.transpose(1, 2))
+
+    if presence is not None:
+        routing -= (1. - presence.unsqueeze(-2)) * 1e32
+
+    routing = F.softmax(routing / np.sqrt(d_k), -1)
+
+    # every output is a linear combination of all inputs
+    # [B, N, M] x [B, M, d_v] = [B, N, d_v]
+    out = torch.matmul(routing, values)
+    return out
+
+
+class MultiHeadQKVAttention(nn.Module):
+    """Multi-head version of Transformer-like attention."""
+
+    def __init__(self, d_k, d_v, n_heads):
+        super().__init__()
+        self.d_k = d_k
+        self.d_v = d_v
+        self._n_heads = n_heads
+
+        self.d_k_s = int(math.ceil(d_k / n_heads))
+        self.d_v_s = int(math.ceil(d_v / n_heads))
+        d_k_p = self.d_k_s * n_heads
+        d_v_p = self.d_v_s * n_heads
+
+        self.q_projector = nn.Linear(d_k, d_k_p)
+        self.k_projector = nn.Linear(d_k, d_k_p)
+        self.v_projector = nn.Linear(d_v, d_v_p)
+        self.o_projector = nn.Linear(d_v_p, d_v)
+
+    def forward(self, queries, keys, values, presence=None):
+        """
+        Multi-head transformer-like self-attention.
+
+        Args:
+          queries: Tensor of shape [B, N, d_k].
+          keys: Tensor of shape [B, M, d_k].
+          values: : Tensor of shape [B, M, d_v].
+          presence: None or tensor of shape [B, M].
+
+        Returns:
+          Tensor of shape [B, N, d_v]
+        """
+        assert queries.shape[2] == keys.shape[2]
+        assert keys.shape[1] == values.shape[1]
+        if presence is not None:
+            assert values.shape[:2] == presence.shape
+
+        batch_size = queries.shape[0]
+
+        q_p = self.q_projector(queries)  # (B, N, d_k_p)
+        k_p = self.k_projector(keys)  # (B, M, d_k_p)
+        v_p = self.v_projector(values)  # (B, M, d_v_p)
+
+        q_s = q_p.split(self.d_k_s, -1)  # [(B, N, d_k_s)] * H
+        k_s = k_p.split(self.d_k_s, -1)  # [(B, M, d_k_s)] * H
+        v_s = v_p.split(self.d_v_s, -1)  # [(B, M, d_v_s)] * H
+
+        q = torch.cat(q_s, 0)  # (H*B, N, d_k_s)
+        k = torch.cat(k_s, 0)  # (H*B, M, d_k_s)
+        v = torch.cat(v_s, 0)  # (H*B, M, d_v_s)
+
+        if presence is not None:
+            presence = presence.repeat(self._n_heads, 1)
+        o = qkv_attention(q, k, v, presence)  # (H*B, N, d_v_s)
+        o = o.split(batch_size, 0)  # [(B, N, d_v_s)] * H
+        o = torch.cat(o, -1)  # (B, N, d_v_p)
+
+        out = self.o_projector(o)  # (B, N, d_v)
+        return out
+
+
+class MAB(nn.Module):
+    def __init__(self, d, n_heads, layer_norm=False):
+        super().__init__()
+        self.mqkv = MultiHeadQKVAttention(d_k=d, d_v=d, n_heads=n_heads)
+        self.layer_norm = layer_norm
+        if layer_norm:
+            self.ln0 = nn.LayerNorm(d)
+            self.ln1 = nn.LayerNorm(d)
+        self.fc = nn.Linear(d, d)
+
+    def forward(self, queries, keys, presence=None):
+        h = self.mqkv(queries, keys, keys, presence)  # (B, N, d)
+        h += queries  # (B, N, d)
+        if self.layer_norm:
+            h = self.ln0(h)  # (B, N, d)
+
+        h += F.relu(self.fc(h))  # (B, N, d)
+        if self.layer_norm:
+            h = self.ln1(h)  # (B, N, d)
+
+        return h
+
+
+class SAB(nn.Module):
+    def __init__(self, d, n_heads, layer_norm=False):
+        super().__init__()
+        self.mab = MAB(d=d, n_heads=n_heads, layer_norm=layer_norm)
+
+    def forward(self, x, presence=None):
+        return self.mab(x, x, presence)
+
+
+class ISAB(nn.Module):
+    def __init__(self, d, n_heads, n_inducing_points, layer_norm=False):
+        super().__init__()
+        self.mab0 = MAB(d=d, n_heads=n_heads, layer_norm=layer_norm)
+        self.mab1 = MAB(d=d, n_heads=n_heads, layer_norm=layer_norm)
+        self.I = nn.Parameter(torch.zeros(1, n_inducing_points, d),
+                              requires_grad=True)
+        with torch.no_grad():
+            nn.init.xavier_uniform_(self.I)
+
+    def forward(self, x, presence=None):
+        batch_size = x.shape[0]
+        H = self.mab0(self.I.repeat(batch_size, 1, 1), x, presence)
+        return self.mab1(x, H)
+
+
+class PMA(nn.Module):
+    def __init__(self, d, n_heads, n_seeds, layer_norm=False):
+        super().__init__()
+        self.mab = MAB(d=d, n_heads=n_heads, layer_norm=layer_norm)
+        self.S = nn.Parameter(torch.zeros(1, n_seeds, d), requires_grad=True)
+        with torch.no_grad():
+            nn.init.xavier_uniform_(self.S)
+
+    def forward(self, x, presence=None):
+        batch_size = x.shape[0]
+        return self.mab(self.S.repeat(batch_size, 1, 1), x, presence)
+
+
+class SetTransformer(nn.Module):
+    """Permutation-invariant Transformer."""
+
+    def __init__(self,
+                 dim_in,
+                 dim_hidden,
+                 dim_out,
+                 n_outputs,
+                 n_layers,
+                 n_heads,
+                 layer_norm=False,
+                 n_inducing_points: int = None):
+        super().__init__()
+
+        self.fc1 = nn.Linear(dim_in, dim_hidden)
+
+        args = dict(
+            d=dim_hidden,
+            n_heads=n_heads,
+            layer_norm=layer_norm,
+        )
+        if n_inducing_points is None:
+            sab_fn = SAB
+        else:
+            args['n_inducing_points'] = n_inducing_points
+            sab_fn = ISAB
+        layers = [sab_fn(**args) for _ in range(n_layers)]
+        self.sabs = nn.ModuleList(layers)
+
+        self.fc2 = nn.Linear(dim_hidden, dim_out)
+
+        self.pma = PMA(d=dim_out,
+                       n_heads=n_heads,
+                       n_seeds=n_outputs,
+                       layer_norm=layer_norm)
+
+    def forward(self, x, presence=None):
+        h = self.fc1(x)
+
+        for sab in self.sabs:
+            h = sab(h, presence)
+
+        z = self.fc2(h)
+
+        return self.pma(z, presence)
---
+++ torch_scae/test_set_transformer.py
@@ -0,0 +1,177 @@
+import unittest
+
+import torch
+
+from torch_scae.set_transformer import qkv_attention, MultiHeadQKVAttention,\
+    MAB, SAB, ISAB, PMA, SetTransformer
+
+
+class QKVAttentionTestCase(unittest.TestCase):
+    def test_qkv_attention(self):
+        B = 32
+        d_k = 16
+        d_v = 32
+        N = 10
+        M = 40
+
+        q = torch.rand(B, N, d_k)
+        k = torch.rand(B, M, d_k)
+        v = torch.rand(B, M, d_v)
+        presence = torch.rand(B, M)
+        out = qkv_attention(q, k, v, presence)
+
+        self.assertTrue(out.shape == (B, N, d_v))
+
+    def test_multi_head_qkv_attention(self):
+        B = 32
+        d_k = 16
+        d_v = 32
+        N = 10
+        M = 40
+        n_heads = 3
+
+        q = torch.rand(B, N, d_k)
+        k = torch.rand(B, M, d_k)
+        v = torch.rand(B, M, d_v)
+        presence = torch.rand(B, M)
+
+        mhqkv = MultiHeadQKVAttention(d_k=d_k, d_v=d_v, n_heads=n_heads)
+        out = mhqkv(q, k, v, presence)
+
+        self.assertTrue(out.shape == (B, N, d_v))
+
+    def test_mab(self):
+        B = 32
+        d = 16
+        N = 10
+        M = 40
+        n_heads = 3
+
+        q = torch.rand(B, N, d)
+        k = torch.rand(B, M, d)
+        presence = torch.rand(B, M)
+
+        mab = MAB(d=d, n_heads=n_heads, layer_norm=False)
+        out = mab(q, k, presence)
+
+        self.assertTrue(out.shape == (B, N, d))
+
+    def test_mab_with_layer_normalization(self):
+        B = 32
+        d = 16
+        N = 10
+        M = 40
+        n_heads = 3
+
+        q = torch.rand(B, N, d)
+        k = torch.rand(B, M, d)
+        presence = torch.rand(B, M)
+
+        mab = MAB(d=d, n_heads=n_heads, layer_norm=True)
+        out = mab(q, k, presence)
+
+        self.assertTrue(out.shape == (B, N, d))
+
+    def test_sab(self):
+        B = 32
+        d = 16
+        N = 10
+        n_heads = 3
+
+        x = torch.rand(B, N, d)
+        presence = torch.rand(B, N)
+
+        sab = SAB(d=d, n_heads=n_heads, layer_norm=True)
+        out = sab(x, presence)
+
+        self.assertTrue(out.shape == (B, N, d))
+
+    def test_isab(self):
+        B = 32
+        d = 16
+        N = 10
+        n_heads = 3
+        n_inducing_points = 5
+
+        x = torch.rand(B, N, d)
+        presence = torch.rand(B, N)
+
+        isab = ISAB(d=d, n_heads=n_heads, n_inducing_points=n_inducing_points,
+                    layer_norm=True)
+        out = isab(x, presence)
+
+        self.assertTrue(out.shape == (B, N, d))
+
+    def test_pma(self):
+        B = 32
+        d = 16
+        N = 10
+        n_heads = 3
+        n_seeds = 10
+
+        x = torch.rand(B, N, d)
+        presence = torch.rand(B, N)
+
+        pma = PMA(d=d, n_heads=n_heads, n_seeds=n_seeds, layer_norm=True)
+        out = pma(x, presence)
+
+        self.assertTrue(out.shape == (B, N, d))
+
+    def test_set_transformer_with_sab(self):
+        B = 32
+        N = 40
+
+        n_heads = 1
+        dim_in = 16
+        dim_hidden = 16
+        dim_out = 256
+        n_outputs = 10
+        n_layers = 3
+
+        x = torch.rand(B, N, dim_in)
+        presence = torch.rand(B, N)
+
+        st = SetTransformer(
+            dim_in,
+            dim_hidden,
+            dim_out,
+            n_outputs,
+            n_layers,
+            n_heads,
+            layer_norm=True,
+        )
+        out = st(x, presence)
+
+        self.assertTrue(out.shape == (B, n_outputs, dim_out))
+
+    def test_set_transformer_with_isab(self):
+        B = 32
+        N = 40
+
+        n_heads = 1
+        dim_in = 16
+        dim_hidden = 16
+        dim_out = 256
+        n_outputs = 10
+        n_layers = 3
+
+        x = torch.rand(B, N, dim_in)
+        presence = torch.rand(B, N)
+
+        st = SetTransformer(
+            dim_in,
+            dim_hidden,
+            dim_out,
+            n_outputs,
+            n_layers,
+            n_heads,
+            layer_norm=True,
+            n_inducing_points=20
+        )
+        out = st(x, presence)
+
+        self.assertTrue(out.shape == (B, n_outputs, dim_out))
+
+
+if __name__ == '__main__':
+    unittest.main()
