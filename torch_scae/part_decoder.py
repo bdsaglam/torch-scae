60dfae9fbcb19b28a7d9969ebae5ab3ea235aa19
Barış Deniz Sağlam <bdsaglam@gmail.com>
2020-05-20 07:49:29 +0300

    Separated template generation and decoding.

--- torch_scae/part_decoder.py
+++ torch_scae/part_decoder.py
@@ -10,48 +10,30 @@ from torch_scae.general_utils import prod
 from torch_scae.nn_ext import relu1, MLP
 
 
-class TemplateBasedImageDecoder(nn.Module):
+class TemplateGenerator(nn.Module):
     """Template-based primary capsule decoder for images."""
-
-    _templates = None
 
     def __init__(self,
                  n_templates,
+                 n_channels,
                  template_size,
-                 output_size,
-                 n_channels=1,
-                 n_template_features=None,
-                 learn_output_scale=False,
-                 colorize_templates=False,
                  template_nonlin=relu1,
-                 color_nonlin=relu1,
-                 use_alpha_channel=False,
-                 background_image=True):
+                 dim_feature=None,
+                 colorize_templates=False,
+                 color_nonlin=relu1):
 
         super().__init__()
         self._n_templates = n_templates
         self._template_size = template_size
         self._n_channels = n_channels
-        self._output_size = output_size
-        self._learn_output_scale = learn_output_scale
+        self._template_nonlin = template_nonlin
+        self._dim_feature = dim_feature
         self._colorize_templates = colorize_templates
         self._color_nonlin = color_nonlin
-        self._n_template_features = n_template_features
-        self._template_nonlin = template_nonlin
-        self._use_alpha_channel = use_alpha_channel
-        self._background_image = background_image
 
         self._build()
 
     def _build(self):
-        self._setup_templates()
-        self.bg_mixing_logit = nn.Parameter(torch.tensor([0.0]),
-                                            requires_grad=True)
-        if self._background_image:
-            self.bg_value = nn.Parameter(torch.tensor([0.0]),
-                                         requires_grad=True)
-
-    def _setup_templates(self):
         # create templates
         template_shape = (
             1, self._n_templates, self._n_channels, *self._template_size
@@ -68,47 +50,29 @@ class TemplateBasedImageDecoder(nn.Module):
         self.template_logits = nn.Parameter(torch.from_numpy(q),
                                             requires_grad=True)
 
-        if self._use_alpha_channel:
-            shape = (1, self._n_templates, 1, *self._template_size)
-            self.templates_alpha = nn.Parameter(torch.zeros(*shape),
-                                                requires_grad=True)
-        else:
-            self.temperature_logit = nn.Parameter(torch.rand(1),
-                                                  requires_grad=True)
-
         if self._colorize_templates:
             self.templates_color_mlp = MLP(
-                sizes=[self._n_template_features, 32, self._n_channels]
-            )
+                sizes=[self._dim_feature, 32, self._n_channels])
 
-        if self._learn_output_scale:
-            self.scale = nn.Parameter(torch.rand(1), requires_grad=True)
-
-    def forward(self,
-                pose,
-                presence=None,
-                template_feature=None,
-                bg_image=None):
+    def forward(self, feature=None, batch_size=None):
         """Builds the module.
 
         Args:
-          pose: [B, n_templates, 6] tensor.
-          presence: [B, n_templates] tensor.
-          template_feature: [B, n_templates, n_features] tensor; these features
+          feature: [B, n_templates, dim_feature] tensor; these features
           are used to change templates based on the input, if present.
-          bg_image: [B, *output_size] tensor representing the background.
 
         Returns:
-          [B, n_templates, *output_size, n_channels] tensor.
+          (B, n_templates, n_channels, *template_size) tensor.
         """
-        device = next(iter(self.parameters())).device
-        batch_size, n_templates = list(pose.shape[:2])
+        if feature is not None:
+            batch_size = feature.shape[0]
 
         raw_templates = self._template_nonlin(self.template_logits)
 
-        if self._colorize_templates and template_feature is not None:
+        if self._colorize_templates and feature is not None:
+            n_templates = feature.shape[1]
             template_color = self.templates_color_mlp(
-                template_feature.view(batch_size * n_templates, -1)
+                feature.view(batch_size * n_templates, -1)
             )  # (BxM, C)
             if self._color_nonlin == relu1:
                 template_color += .99
@@ -117,22 +81,87 @@ class TemplateBasedImageDecoder(nn.Module):
                 batch_size, n_templates, template_color.shape[1]
             )  # (B, M, C)
             templates = raw_templates * template_color[:, :, :, None, None]
+        else:
+            templates = raw_templates.repeat(batch_size, 1, 1, 1, 1)
 
-        if templates.shape[0] == 1:
-            templates = templates.repeat(batch_size, 1, 1, 1, 1)
+        return AttrDict(
+            raw_templates=raw_templates,
+            templates=templates,
+        )
+
+
+class TemplateBasedImageDecoder(nn.Module):
+    """Template-based primary capsule decoder for images."""
+
+    def __init__(self,
+                 n_templates,
+                 template_size,
+                 output_size,
+                 learn_output_scale=False,
+                 use_alpha_channel=False,
+                 background_value=True):
+
+        super().__init__()
+        self._n_templates = n_templates
+        self._template_size = template_size
+        self._output_size = output_size
+        self._learn_output_scale = learn_output_scale
+        self._use_alpha_channel = use_alpha_channel
+        self._background_value = background_value
+
+        self._build()
+
+    def _build(self):
+        if self._use_alpha_channel:
+            shape = (1, self._n_templates, 1, *self._template_size)
+            self.templates_alpha = nn.Parameter(torch.zeros(*shape),
+                                                requires_grad=True)
+        else:
+            self.temperature_logit = nn.Parameter(torch.rand(1),
+                                                  requires_grad=True)
+
+        if self._learn_output_scale:
+            self.scale = nn.Parameter(torch.rand(1), requires_grad=True)
+
+        self.bg_mixing_logit = nn.Parameter(torch.tensor([0.0]),
+                                            requires_grad=True)
+        if self._background_value:
+            self.bg_value = nn.Parameter(torch.tensor([0.0]),
+                                         requires_grad=True)
+
+    def forward(self,
+                templates,
+                pose,
+                presence=None,
+                bg_image=None):
+        """Builds the module.
+
+        Args:
+          templates: (B, n_templates, n_channels, *template_size) tensor
+          pose: [B, n_templates, 6] tensor.
+          presence: [B, n_templates] tensor.
+          bg_image: [B, n_channels, *output_size] tensor representing the background.
+
+        Returns:
+          (B, n_templates, n_channels, *output_size) tensor.
+        """
+        device = next(iter(self.parameters())).device
+
+        # B, M, C, H, W
+        batch_size, n_templates, n_channels, height, width = templates.shape
 
         # transform templates
         templates = templates.view(batch_size * n_templates,
-                                   *templates.shape[2:])
-        affine_matrices = pose.view(batch_size * n_templates, 2, 3)
+                                   *templates.shape[2:])  # (B*M, C, H, W)
+        affine_matrices = pose.view(batch_size * n_templates, 2, 3) # (B*M, 2, 3)
         target_size = [
-            batch_size * n_templates, self._n_channels, *self._output_size]
+            batch_size * n_templates, n_channels, *self._output_size]
         affine_grids = F.affine_grid(affine_matrices, target_size)
         transformed_templates = F.grid_sample(templates, affine_grids)
         transformed_templates = transformed_templates.view(
             batch_size, n_templates, *target_size[1:])
 
-        #
+        # background image
         if bg_image is not None:
             bg_image = bg_image.unsqueeze(1)
         else:
@@ -169,9 +198,10 @@ class TemplateBasedImageDecoder(nn.Module):
         else:
             scale = 1
 
-        presence = presence.view(*presence.shape,
-                                 *([1] * len(template_mixing_logits.shape[2:])))
-        template_mixing_logits += math_ops.log_safe(presence)
+        if presence is not None:
+            presence = presence.view(
+                *presence.shape, *([1] * len(template_mixing_logits.shape[2:])))
+            template_mixing_logits += math_ops.log_safe(presence)
 
         rec_pdf = GaussianMixture.make_from_stats(
             loc=transformed_templates,
@@ -180,7 +210,6 @@ class TemplateBasedImageDecoder(nn.Module):
         )
 
         return AttrDict(
-            raw_templates=raw_templates,
             transformed_templates=transformed_templates[:, :-1],
             mixing_logits=template_mixing_logits[:, :-1],
             pdf=rec_pdf,
--- torch_scae/test_part_decoder.py
+++ torch_scae/test_part_decoder.py
@@ -2,54 +2,167 @@ import unittest
 
 import torch
 
-from torch_scae.configs import mnist_config
-from torch_scae.part_decoder import TemplateBasedImageDecoder
-from torch_scae.part_encoder import CNNEncoder, CapsuleImageEncoder
+from torch_scae.part_decoder import TemplateGenerator, TemplateBasedImageDecoder
 
 
-class TemplateBasedImageDecoderTestCase(unittest.TestCase):
-    def test_init(self):
-        config = mnist_config
-        cnn_encoder = CNNEncoder(
-            **config.pcae_cnn_encoder
+class TemplateGeneratorTestCase(unittest.TestCase):
+    def helper(self,
+               image_shape=(1, 28, 28),
+               n_templates=40,
+               template_size=(11, 11),
+               colorize_templates=True):
+
+        if colorize_templates:
+            dim_feature = 16
+        else:
+            dim_feature = None
+
+        n_channels = image_shape[0]
+        template_nonlin = torch.sigmoid
+        color_nonlin = torch.sigmoid
+
+        template_generator = TemplateGenerator(
+            n_templates=n_templates,
+            n_channels=n_channels,
+            template_size=template_size,
+            template_nonlin=template_nonlin,
+            dim_feature=dim_feature,
+            colorize_templates=colorize_templates,
+            color_nonlin=color_nonlin,
         )
-        capsule_image_encoder = CapsuleImageEncoder(
-            encoder=cnn_encoder,
-            **config.pcae_primary_capsule
-        )
-
-        template_decoder = TemplateBasedImageDecoder(
-            **config.pcae_template_decoder)
-
-        n_templates = config.pcae_template_decoder.n_templates
-        template_size = config.pcae_template_decoder.template_size
-        n_channels = config.pcae_template_decoder.n_channels
-        input_shape = config.pcae_primary_capsule.input_shape
-        output_size = config.pcae_template_decoder.output_size
-        self.assertTrue(input_shape[1:] == output_size)
 
         with torch.no_grad():
             batch_size = 4
-            image = torch.rand(batch_size, *config.image_shape)
-            encoding_result = capsule_image_encoder(image)
-            decoding_result = template_decoder(
-                pose=encoding_result.pose,
-                presence=encoding_result.presence,
-                template_feature=encoding_result.feature,
+            if colorize_templates:
+                feature = torch.rand(batch_size, n_templates, dim_feature)
+                res = template_generator(feature)
+            else:
+                res = template_generator(batch_size=batch_size)
+
+            self.assertTrue(
+                res.raw_templates.shape == (
+                    1, n_templates, n_channels, *template_size)
             )
-            raw_templates = decoding_result.raw_templates
+            self.assertTrue(
+                res.templates.shape == (
+                    batch_size, n_templates, n_channels, *template_size)
+            )
+
+    def test_shape_single_channel_without_color(self):
+        self.helper(image_shape=(1, 28, 28),
+                    colorize_templates=False)
+
+    def test_shape_single_channel_with_color(self):
+        self.helper(image_shape=(1, 28, 28),
+                    colorize_templates=True)
+
+    def test_shape_multi_channel_without_color(self):
+        self.helper(image_shape=(3, 28, 28),
+                    colorize_templates=False)
+
+    def test_shape_multi_channel_with_color(self):
+        self.helper(image_shape=(3, 28, 28),
+                    colorize_templates=True)
+
+    def test_shape_with_different_template_size(self):
+        image_shape = (1, 28, 28)
+        self.helper(image_shape=image_shape, template_size=(11, 11))
+        self.helper(image_shape=image_shape, template_size=(22, 22))
+        self.helper(image_shape=image_shape, template_size=(32, 32))
+
+
+class TemplateBasedImageDecoderTestCase(unittest.TestCase):
+    def helper(self,
+               image_shape=(1, 28, 28),
+               n_templates=40,
+               template_size=(11, 11),
+               learn_output_scale=False,
+               use_alpha_channel=True,
+               background_value=True,
+               presence=True,
+               background_image=True,
+               ):
+        n_channels = image_shape[0]
+        output_size = image_shape[1:]
+
+        template_decoder = TemplateBasedImageDecoder(
+            n_templates=n_templates,
+            template_size=template_size,
+            output_size=output_size,
+            learn_output_scale=learn_output_scale,
+            use_alpha_channel=use_alpha_channel,
+            background_value=background_value,
+        )
+
+        with torch.no_grad():
+            batch_size = 4
+
+            templates = torch.rand(
+                batch_size, n_templates, n_channels, *template_size)
+
+            pose = torch.rand(batch_size, n_templates, 6)
+
+            if presence:
+                presence = torch.rand(batch_size, n_templates)
+            else:
+                presence = None
+
+            if background_image:
+                bg_image = torch.rand(batch_size, n_channels, *output_size)
+            else:
+                bg_image = None
+
+            decoding_result = template_decoder(templates=templates,
+                                               pose=pose,
+                                               presence=presence,
+                                               bg_image=bg_image)
             transformed_templates = decoding_result.transformed_templates
             mixing_logits = decoding_result.mixing_logits
-            self.assertTrue(
-                raw_templates.shape == (1, n_templates, n_channels, *template_size)
-            )
+
             self.assertTrue(
                 transformed_templates.shape == (
                     batch_size, n_templates, n_channels, *output_size)
             )
             self.assertTrue(
-                mixing_logits.shape == (batch_size, n_templates, n_channels, *output_size)
+                mixing_logits.shape == (
+                    batch_size, n_templates, 1, *output_size)
             )
+
+    def test_shape_with_color(self):
+        self.helper(image_shape=(3, 28, 28))
+
+    def test_shape_without_color(self):
+        self.helper(image_shape=(1, 28, 28))
+
+    def test_shape_with_scale(self):
+        self.helper(learn_output_scale=True)
+
+    def test_shape_without_scale(self):
+        self.helper(learn_output_scale=False)
+
+    def test_shape_with_alpha(self):
+        self.helper(use_alpha_channel=True)
+
+    def test_shape_without_alpha(self):
+        self.helper(use_alpha_channel=False)
+
+    def test_shape_with_presence(self):
+        self.helper(presence=True)
+
+    def test_shape_without_presence(self):
+        self.helper(presence=False)
+
+    def test_shape_with_bg_value(self):
+        self.helper(background_value=True)
+
+    def test_shape_without_bg_value(self):
+        self.helper(background_value=False)
+
+    def test_shape_with_bg_image(self):
+        self.helper(background_image=True)
+
+    def test_shape_without_bg_image(self):
+        self.helper(background_image=False)
 
 
 if __name__ == '__main__':
