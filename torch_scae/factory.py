1c83f31930d7952068b0974ab50130ab4f1a7d83
Barış Deniz Sağlam <bdsaglam@gmail.com>
2020-05-20 12:12:02 +0300

    Added a factory method for SCAE.

---
+++ torch_scae/factory.py
@@ -0,0 +1,32 @@
+from torch_scae.object_decoder import CapsuleLayer, CapsuleObjectDecoder
+from torch_scae.part_decoder import TemplateGenerator, TemplateBasedImageDecoder
+from torch_scae.part_encoder import CNNEncoder, CapsuleImageEncoder
+from torch_scae.set_transformer import SetTransformer
+from torch_scae.stacked_capsule_auto_encoder import SCAE
+
+
+def make_scae_from_config(config):
+    cnn_encoder = CNNEncoder(**config.pcae_cnn_encoder)
+    part_encoder = CapsuleImageEncoder(
+        encoder=cnn_encoder,
+        **config.pcae_encoder
+    )
+
+    template_generator = TemplateGenerator(**config.pcae_template_generator)
+    part_decoder = TemplateBasedImageDecoder(**config.pcae_decoder)
+
+    obj_encoder = SetTransformer(**config.ocae_encoder_set_transformer)
+
+    obj_decoder_capsule = CapsuleLayer(**config.ocae_decoder_capsule)
+    obj_decoder = CapsuleObjectDecoder(obj_decoder_capsule)
+
+    scae = SCAE(
+        part_encoder=part_encoder,
+        template_generator=template_generator,
+        part_decoder=part_decoder,
+        obj_encoder=obj_encoder,
+        obj_decoder=obj_decoder,
+        **config.scae
+    )
+
+    return scae
