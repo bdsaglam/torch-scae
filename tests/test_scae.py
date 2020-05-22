0eb3346c40f99d18de81dfd07cd1317876d72a68
Barış Deniz Sağlam <bdsaglam@gmail.com>
2020-05-22 12:41:42 +0300

    Config fix in tests.

--- tests/test_scae.py
+++ tests/test_scae.py
@@ -2,12 +2,12 @@ import unittest
 
 import torch
 
+from torch_scae.configs import mnist_config
 from torch_scae.object_decoder import CapsuleLayer, CapsuleObjectDecoder
 from torch_scae.part_decoder import TemplateBasedImageDecoder, TemplateGenerator
 from torch_scae.part_encoder import CNNEncoder, CapsuleImageEncoder
 from torch_scae.set_transformer import SetTransformer
 from torch_scae.stacked_capsule_auto_encoder import SCAE
-from . import mnist_config
 
 
 class SCAETestCase(unittest.TestCase):
