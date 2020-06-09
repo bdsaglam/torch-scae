4023d93be85452cbfe0b8ec3f754fe6818084e38
Barış Deniz Sağlam <bdsaglam@gmail.com>
2020-06-09 12:43:20 +0300

    Increment version number.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='torch_scae',
-    version=0.3,
+    version=0.4,
     packages=find_packages(),
     install_requires=[
         'monty>=3.0.2',
