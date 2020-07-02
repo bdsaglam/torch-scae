b6c533819374f68a6dd17e7060e5f607e53cee25
Barış Deniz Sağlam <bdsaglam@gmail.com>
2020-07-02 11:02:11 +0300

    Fix versions of required packages.

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ setup(
     version=0.4,
     packages=find_packages(),
     install_requires=[
-        'monty>=3.0.2',
-        'numpy>=1.17.2',
-        'torch>=1.4.0',
-        'torchvision>=0.5.0',
-        'pytorch-lightning>=0.7.6',
+        'monty==3.0.2',
+        'numpy==1.17.2',
+        'torch==1.4.0',
+        'torchvision==0.5.0',
+        'pytorch-lightning==0.7.6',
     ],
 )
